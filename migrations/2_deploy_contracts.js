737b5a8883a1bb6621e84c5b157cbeb87ce1484b
Shehyaaz <shehyaaz.cs17@bmsce.ac.in>
2021-03-19 18:45:31 +0530

    Added outline of smart contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const Constants = artifacts.require("./Constants.sol");
+const CheckInterface = artifacts.require("./CheckInterface.sol");
+const DRPReactionInterface = artifacts.require("./DRPReactionInterface.sol");
+const Check = artifacts.require("./Check.sol");
+const DRPReaction = artifacts.require("./DRPReaction.sol");
+const DDA = artifacts.require("./DDA.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+ deployer.deploy(Constants);
+ deployer.deploy(CheckInterface);
+ deployer.deploy(DRPReactionInterface);
+ deployer.deploy(Check);
+ deployer.deploy(DRPReaction);
+ deployer.deploy(DDA);
 };
